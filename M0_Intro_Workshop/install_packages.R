c3caf3229fe4c220d2769cd8803a2489bfb654b3
bacorli2 <150067594+bacorli2@users.noreply.github.com>
2024-01-08 16:53:22 -0500

    Added code to test packages, includes basic seurat pipeline.

---
+++ M0_Intro_Workshop/install_packages.R
@@ -0,0 +1,22 @@
+
+
+# Base Packages
+base_packages <- c("tidyverse", "BiocManager", "cowplot", "here", "R.utils", "HGNChelper","openxlsx")
+base_installs <- install.packages(setdiff(base_packages, rownames(installed.packages())), 
+                                  dependencies = TRUE)
+# Load all base packages
+base_loads <- lapply(base_packages, library, character.only = TRUE)
+# Bioconductor Packages
+biocm_packages <-  c("BiocParallel", "Seurat", "SeuratWrappers","monocle","celldex")
+bioc_installs <- setdiff(biocm_packages, rownames(installed.packages()))
+if (length(bioc_installs)) {BiocManager::install(bioc_installs) }
+# Load all Bioconductor packages
+bioc_loads <- lapply(biocm_packages, library, character.only = TRUE)
+
+
+# Set base directory of for workshop
+here::i_am("README.md")
+
+
+# Create data directory for workshop
+dir.create(here::here("temp_data"))
